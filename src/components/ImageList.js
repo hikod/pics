03a847a0bd0ade111132ca92dc4101c71cc90995
hdogrul <hdogrul@HD-MacBook-Pro.local>
2022-02-08 00:02:03 -0500

    changing url size

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,10 +1,11 @@
 import React from "react";
 
 const ImageList = (props) => {
+  console.log(props.images);
   return (
     <div>
       {props.images.map(({ id, description, urls }) => {
-        return <img src={urls.thumb} alt={description} key={id} />;
+        return <img src={urls.regular} alt={description} key={id} />;
       })}
     </div>
   );
