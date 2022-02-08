ffb9e5f9853d02dbd256771c362f7c011f0aa18d
hdogrul <hdogrul@HD-MacBook-Pro.local>
2022-02-07 23:32:54 -0500

    adding image list component

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,7 +1,14 @@
 import React from "react";
 
-const ImageList = () => {
-  return <div></div>;
+const ImageList = (props) => {
+  console.log(props.images);
+  return (
+    <div>
+      {props.images.map(({ id, description, urls }) => {
+        return <img src={urls.thumb} alt={description} key={id} />;
+      })}
+    </div>
+  );
 };
 
 export default ImageList;
